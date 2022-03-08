e2f738cff83aa4f6de7122e6b31db2603fae1bde
silhavyj <jakub.sil@seznam.cz>
2022-03-08 18:14:40 +0100

    Initial design of the main window

---
+++ src/main/java/cz/zcu/kiv/ir/silhavyj/searchengine/Main.java
@@ -0,0 +1,32 @@
+package cz.zcu.kiv.ir.silhavyj.searchengine;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private static final String TITLE = "KIV/IR - Search Engine - silhavyj";
+    private static final String VIEW_FXML_FILE = "view.fxml";
+
+    private static final int WINDOW_MIN_WIDTH = 720;
+    private static final int WINDOW_MIN_HEIGHT = 480;
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(VIEW_FXML_FILE));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle(TITLE);
+        stage.setScene(scene);
+        stage.setMinHeight(WINDOW_MIN_HEIGHT);
+        stage.setMinWidth(WINDOW_MIN_WIDTH);
+        stage.show();
+    }
+}
--- src/main/java/cz/zcu/kiv/ir/silhavyj/searchengine/MainController.java
+++ src/main/java/cz/zcu/kiv/ir/silhavyj/searchengine/MainController.java
@@ -3,12 +3,13 @@ package cz.zcu.kiv.ir.silhavyj.searchengine;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
+public class MainController {
+
+    /*@FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    }*/
 }
