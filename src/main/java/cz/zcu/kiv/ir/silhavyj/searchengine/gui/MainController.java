e7ad04458ea8f4ecab3767b7bdedcdba02c03abd
silhavyj <jakub.sil@seznam.cz>
2022-03-22 09:23:41 +0100

    Started commenting MainController.java

--- src/main/java/cz/zcu/kiv/ir/silhavyj/searchengine/gui/MainController.java
+++ src/main/java/cz/zcu/kiv/ir/silhavyj/searchengine/gui/MainController.java
@@ -35,65 +35,108 @@ import java.util.*;
 import static com.github.pemistahl.lingua.api.Language.*;
 import static java.time.LocalDateTime.now;
 
+/***
+ * @author Jakub Silhavy
+ *
+ * This class handles all inputs from the user.
+ */
 public class MainController implements Initializable {
 
+    /*** Folder to store documents the user fetched from the BBC News*/
     private static final String FETCHED_DATA_FOLDER = "fetched-data";
+
+    /*** File containing Czech stop words */
     private static final String STOPWORDS_CZECH = "stopwords-cs.txt";
+
+    /*** File containing English stop words */
     private static final String STOPWORDS_ENGLISH = "stopwords-en.txt";
 
+    /*** Bar menu in the top left corner */
     @FXML
     private MenuBar menuBar;
 
+    /*** Menu item for adding documents from the disk */
     @FXML
     private MenuItem addJSONDocumentMenuItem;
 
+    /*** Search button to perform a search */
     @FXML
     private Button searchBtn;
 
+    /*** Status label (information about what's going on, error messages, etc.) */
     @FXML
     private Label statusLabel;
 
+    /*** Text field where the user inputs a search query */
     @FXML
     private TextField queryTextField;
 
+    /*** Tab pane holding the results of a search */
     @FXML
     private TabPane resultsTabPane;
 
+    /*** Slider the user uses to adjust the number of displayed results of a document */
     @FXML
     private Slider topResultsCountSlider;
 
+    /*** Label displaying the maximum of results to be shown to the user */
     @FXML
     private Label topResultsCountLabel;
 
+    /*** TreeView containing information about different indexes within the application */
     @FXML
     private TreeView<String> indexTreeView;
 
+    /*** Radio button to select searching in the Czech language */
     @FXML
     private RadioButton czechLanguageRadioBtn;
 
+    /*** Radio button to select searching in the English language */
     @FXML
     private RadioButton englishLanguageRadioBtn;
 
+    /*** Radio button to select ranking by TF-IDF */
     @FXML
     private RadioButton tfidfRadioButton;
 
+    /*** Radio button to select ranking by cosine similarity */
     @FXML
     private RadioButton cosineSimilarityRadioButton;
 
+    /*** Button to terminate importing documents */
     @FXML
     private Button stopLoadingBtn;
 
+    /*** Root item (displaying information about different indexes) */
     private TreeItem<String> treeRootItem;
+
+    /*** Flag if the stop button should be displayed or not */
     private boolean stopDocumentLoading;
 
-    final IQueryParser queryParser = new QueryParseInfix(new QueryLexer());
-    final Map<String, IIndex> languageIndexes = new HashMap<>();
-    final LanguageDetector languageDetector = LanguageDetectorBuilder.fromLanguages(ENGLISH, CZECH, SLOVAK).build();
-    ISiteProcessor siteProcessor;
+    /*** Instance of a query parser */
+    private final IQueryParser queryParser = new QueryParseInfix(new QueryLexer());
 
+    /*** Map of different indexes by their languages */
+    private final Map<String, IIndex> languageIndexes = new HashMap<>();
+
+    /*** Instance of a language detector */
+    private final LanguageDetector languageDetector = LanguageDetectorBuilder.fromLanguages(ENGLISH, CZECH, SLOVAK).build();
+
+    /*** Instance of a site processor */
+    private ISiteProcessor siteProcessor;
+
+    /***
+     * Initializes the class.
+     * @param url unused
+     * @param resourceBundle unused
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Instantiate the site processor
         siteProcessor = new BBCNewsProcessor();
+
+        // Bind the slider and the label (number of results to
+        // be displayed to the user).
         topResultsCountSlider.valueProperty().addListener((observableValue, oldValue, newValue) -> {
             if (newValue == null) {
                 topResultsCountLabel.setText("null");
@@ -102,11 +145,21 @@ public class MainController implements Initializable {
             topResultsCountLabel.setText(String.format("(%d)", Math.round(newValue.intValue())));
         });
 
+        // Create the root node of the tree view.
         treeRootItem = new TreeItem<>("index");
         indexTreeView.setRoot(treeRootItem);
+
+        // Hide the stop button.
         stopLoadingBtn.setVisible(false);
     }
 
+    /***
+     * Disables any user input.
+     * This method is called after the user submits documents
+     * to be added to the application. All user inputs are enabled
+     * either when the insertion finishes or when they click the stop button.
+     * @param disable flag if the user input should be enabled or disabled
+     */
     private void disableUserInput(boolean disable) {
         addJSONDocumentMenuItem.setDisable(disable);
         searchBtn.setDisable(disable);
@@ -114,6 +167,14 @@ public class MainController implements Initializable {
         stopLoadingBtn.setVisible(disable);
     }
 
+    /***
+     * Returns a JSON document by a given index.
+     * It gets the path of the document on the disk, reads it,
+     * and parses it.
+     * @param documentIndex index of the document to be parsed
+     * @param index Index in which the document is held
+     * @return JSON object, if all goes well. Null, otherwise.
+     */
     private JSONObject getJSONDocument(int documentIndex, final IIndex index) {
         final String rawData = IOUtils.readFile(index.getFilePath(documentIndex));
         try {
@@ -124,15 +185,29 @@ public class MainController implements Initializable {
         return null;
     }
 
+    /***
+     * Displays the results of a search to the user.
+     * Each document is displayed in a separate tab.
+     * @param resultIndexer List of documents matching the query (sorted)
+     * @param ranks map of ranks (each document has its own value)
+     * @param count maximum number of documents to be displayed
+     * @param timeOfSearchInMS how much time the query took in milliseconds
+     * @param index Index in which the search was performed
+     * @param language Language in which the search was performed
+     */
     private void displayResults(final List<Integer> resultIndexer, final HashMap<Integer, Double> ranks, int count, long timeOfSearchInMS, final IIndex index, final Language language) {
         JSONObject data;
         int totalNumberOfDocument = 0;
-
         int documentIndex;
         final var iter = resultIndexer.listIterator();
 
+        // Iterate through all documents and display them to the user.
         while (iter.hasNext() && count > 0) {
+            // Get the current document index.
             documentIndex = iter.next();
+
+            // Read the documents from the disk, and if it was loaded successfully,
+            // create a new tab for it.
             data = getJSONDocument(documentIndex, index);
             if (data != null) {
                 final var resultTab = createResultTab(data, documentIndex, index, ranks);
@@ -141,6 +216,9 @@ public class MainController implements Initializable {
             count--;
             totalNumberOfDocument++;
         }
+
+        // Finish calculating how many results we found in total and
+        // display it into the status bar (label).
         while (iter.hasNext()) {
             totalNumberOfDocument++;
             iter.next();
@@ -149,6 +227,14 @@ public class MainController implements Initializable {
         statusLabel.setText(language + " - found " + totalNumberOfDocument + " matching documents (" + timeOfSearchInMS + "ms)");
     }
 
+    /***
+     * Creates a new tab for a given document
+     * @param data document in a JSON format (it contains the text as well as its metadata)
+     * @param documentIndex index of the document
+     * @param index Index in which the document is held
+     * @param ranks map of ranks
+     * @return new tab holding information about the document
+     */
     private Tab createResultTab(final JSONObject data, int documentIndex, final IIndex index, final HashMap<Integer, Double> ranks) {
         Tab tab = new Tab();
         tab.setText(String.valueOf(Paths.get(index.getFilePath(documentIndex)).getFileName()));
@@ -157,32 +243,60 @@ public class MainController implements Initializable {
         return tab;
     }
 
+    /***
+     * Creates the body of a tab that hold information about a document.
+     * @param data document in a JSON format (it contains the text as well as its metadata)
+     * @param documentIndex index of the document
+     * @param index Index in which the document is held
+     * @param ranks map of ranks
+     * @return new body holding information about the document
+     */
     private VBox createTabBody(final JSONObject data, int documentIndex, final IIndex index, final HashMap<Integer, Double> ranks) {
         VBox vBox = new VBox();
         vBox.setSpacing(5);
         vBox.setPadding(new Insets(20, 20, 20, 20));
 
+        // Display the author's name (if present)
         if (data.has("author")) {
             vBox.getChildren().add(createMetadataInfo("Author", (String)data.get("author")));
         }
+
+        // Display the datetime (if present).
         if (data.has("datetime")) {
             vBox.getChildren().add(createMetadataInfo("Datetime", (String)data.get("datetime")));
         }
+
+        // Display the subject (if present).
         if (data.has("subject")) {
             vBox.getChildren().add(createMetadataInfo("Subject", (String)data.get("subject")));
         }
+
+        // Display the rank of the document.
         vBox.getChildren().add(createMetadataInfo("Rank", "" + ranks.get(documentIndex)));
+
+        // Display the location of the document on the disk.
         vBox.getChildren().add(createMetadataInfo("Location", index.getFilePath(documentIndex)));
-        vBox.getChildren().add(createSeparator());
+
+        vBox.getChildren().add(new Separator());
+
+        // Display the title of the document.
         vBox.getChildren().add(createTitle((String)data.get("title")));
+
+        // Display the URL (if present).
         if (data.has("url")) {
             vBox.getChildren().add(createHyperlink((String)data.get("url")));
         }
-        vBox.getChildren().add(createTextArea((String)data.get("article")));
 
+        // Display the content of the document.
+        vBox.getChildren().add(createTextArea((String)data.get("article")));
         return vBox;
     }
 
+    /***
+     * Creates a hyperlink for the URL of a document.
+     * @param url link of the document
+     * @return HBox holding the URL of a document
+     */
     private HBox createHyperlink(final String url) {
         HBox hBox = new HBox();
         hBox.setSpacing(10);
@@ -192,6 +306,12 @@ public class MainController implements Initializable {
         return hBox;
     }
 
+    /***
+     * Creates a piece of metadata about the document (e.g, author, or datetime).
+     * @param key key e.g. 'Author'
+     * @param value value e.g. 'John Smith'
+     * @return HBox containing the piece of metadata
+     */
     private HBox createMetadataInfo(final String key, final String value) {
         HBox hBox = new HBox();
         hBox.setSpacing(10);
@@ -200,25 +320,37 @@ public class MainController implements Initializable {
         return hBox;
     }
 
-    private Separator createSeparator() {
-        return new Separator();
-    }
-
+    /***
+     * Creates a label.
+     * @param text text to be displayed in the label
+     * @param key flag if the label is the key of a piece of metadata e.g. 'Author'
+     * @return create Label
+     */
     private Label createLabel(final String text, boolean key) {
         Label label = new Label();
         label.setText(text);
+
+        // Make it bold if the label is a key.
         if (key) {
             label.setStyle("-fx-font-weight: bold");
         }
         return label;
     }
 
+    /***
+     * Creates the title of a document.
+     * @param title title of the document
+     * @return HBox containing the created title of a document.
+     */
     private HBox createTitle(final String title) {
         HBox hBox = new HBox();
         hBox.setSpacing(10);
         VBox.setMargin(hBox, new Insets(5, 0, 0, 0));
 
+        // Create a label.
         final var titleLabel = createLabel(title, true);
+
+        // Make sure the title is capable of displaying multiple lines.
         titleLabel.setWrapText(true);
         titleLabel.setStyle("-fx-font-size: 18px");
 
